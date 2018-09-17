90000af8d4187de31a8dc8b612d2638217610c6f
Lawrence Angrave <angrave>
2018-09-17 13:42:16 -0500

    Consistently use bash shell for examples

--- example_builds/build_R.sh
+++ example_builds/build_R.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-approot_build https://cloud.r-project.org/src/base/R-3 R 3.5.1 "--with-readline=no"
+# Assumes approot_functions are sourced in ~/.bashrc
+
+# Optional 4 argument is passed to ./configure
+# Example with readline support - 
+#approot_build https://cloud.r-project.org/src/base/R-3 R 3.5.1 "--with-readline=no"
+
+# This might build without man page support...
+approot_build https://cloud.r-project.org/src/base/R-3 R 3.5.1 " --without-man"
--- example_builds/build_readline.sh
+++ example_builds/build_readline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
+#set -e
+# Assumes approot_functions are sourced in ~/.bashrc
+#. ~/approot/approot_functions
+
 approot_build https://ftp.gnu.org/gnu/ncurses ncurses 6.1 
 approot_build https://ftp.gnu.org/gnu/readline readline 7.0 
 approot_build https://curl.haxx.se/download curl 7.61.1
